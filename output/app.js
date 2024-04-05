971538e8518ce71ebff8dcba714fdb85f1125142
devhasanmia <dev.hasanmia@gmail.com>
2024-04-05 09:10:29 +0600

    Type Basics Issue #3 Done!

--- output/app.js
+++ output/app.js
@@ -1,3 +1,20 @@
 "use strict";
-const cosunter = 5200;
-console.log(cosunter);
+console.clear();
+// let playerName = "MD. HASAN MIA";
+// let age = 30;
+// console.log(playerName);
+// console.log(age)
+let multiply = (a, b) => {
+    return a * b;
+};
+console.log(multiply(23, 58));
+// Array
+// let fruits = ["Apple", "Orange", "Mango"];
+// console.log(fruits[1]);
+// objects
+const person = {
+    name: "MD. HASAN MIA",
+    age: 30,
+    city: "Bangladesh",
+};
+console.log(person.name);
